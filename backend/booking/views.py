4f3277ffb8d6030858fcbf08052e2000a9329d84
karthika <karthikapradeep719gmail.com>
2026-02-16 20:16:32 +0530

    Save changes in views.py before merging Karthika-branch

--- backend/booking/views.py
+++ backend/booking/views.py
@@ -1,7 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+
 
 from django.utils.timezone import now
 from datetime import timedelta
